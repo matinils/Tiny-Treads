ee765ddfa40678a7957712ae3aa36484ce097e09
matinils <mkozlowski225@gmail.com>
2020-07-28 16:02:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,17 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Test Window"
 RADIUS = 150
 
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-arcade.set_background_color(arcade.color.BLUE)
-arcade.start_render()
-arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.GREEN)
-arcade.finish_render()
-arcade.run()
+
+class Welcome(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.ALLOY_ORANGE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE)
+
+
+if __name__ == '__main__':
+    app = Welcome()
+    arcade.run()
