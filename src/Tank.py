f4cd7e288c207b56add5fe8e486dbd31a241382e
matinils <mkozlowski225@gmail.com>
2020-07-29 09:06:56 -0500

    Added crosshair and reticle

--- src/Tank.py
+++ src/Tank.py
@@ -5,10 +5,13 @@ class Tank:
 
 	SPRITE_SHEET_PATH = "./../res/sprite-sheet.png"
 
-	def __init__(self, sprite_pos, x, y, angle):
+	def __init__(self, sprite_pos, x, y, angle, max_ad):
 		self.speed = 0
 		self.br_speed = 0
 		self.tr_speed = 0
+		self.max_ad = max_ad
+		self.aim_distance = 0.0
+		self.aim_speed = 0
 		self.turret_lock = True
 
 		self.body_sprite = arcade.Sprite(
@@ -31,6 +34,18 @@ class Tank:
 			x, y
 		)
 
+		self.reticle_sprite = arcade.Sprite(
+			Tank.SPRITE_SHEET_PATH, 1.0,
+			sprite_pos[0] + 85, sprite_pos[1], 13, 13,
+			x, y + 120
+		)
+
+		self.crosshair_sprite = arcade.Sprite(
+			Tank.SPRITE_SHEET_PATH, 1.0,
+			sprite_pos[0] + 75, sprite_pos[1] + 14, 27, 27,
+			x, y + 120
+		)
+
 	def update(self):
 		velocity = (
 			self.speed * math.cos(self.body_sprite.radians),
@@ -40,3 +55,13 @@ class Tank:
 		self.turret_lock_sprite.velocity = velocity
 		self.body_sprite.change_angle = self.br_speed
 		self.turret_sprite.change_angle = self.tr_speed + (0 if self.turret_lock else self.br_speed)
+
+		self.aim_distance += self.aim_speed
+		self.aim_distance = max(min(self.aim_distance, 1.0), 0.0)
+		aim_coord = (120 + (self.max_ad - 120) * self.aim_distance)
+		aim_coord_x = aim_coord * math.cos(self.turret_sprite.radians) + self.turret_sprite.center_x
+		aim_coord_y = aim_coord * math.sin(self.turret_sprite.radians) + self.turret_sprite.center_y
+		self.crosshair_sprite.center_x = aim_coord_x
+		self.crosshair_sprite.center_y = aim_coord_y
+		self.reticle_sprite.center_x = aim_coord_x
+		self.reticle_sprite.center_y = aim_coord_y
--- src/Tiny-Treads.py
+++ src/Tiny-Treads.py
@@ -1,10 +1,9 @@
 import arcade
 from Tank import Tank
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 800
-SCREEN_TITLE = "Test Window"
-RADIUS = 150
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_TITLE = "Tiny Treads"
 
 
 class TinyTreads(arcade.Window):
@@ -44,6 +43,11 @@ class TinyTreads(arcade.Window):
 		if symbol == arcade.key.LSHIFT:
 			self.tanks[0].turret_lock = not self.tanks[0].turret_lock
 			self.tanks[0].turret_lock_sprite.alpha = (255 if self.tanks[0].turret_lock else 0)
+		if symbol == arcade.key.R:
+			self.tanks[0].aim_speed = 0.005
+		if symbol == arcade.key.F:
+			self.tanks[0].aim_speed = -0.005
+
 
 	def on_key_release(self, symbol: int, modifiers: int):
 		if symbol == arcade.key.W or symbol == arcade.key.S:
@@ -52,17 +56,21 @@ class TinyTreads(arcade.Window):
 			self.tanks[0].br_speed = 0
 		if symbol == arcade.key.Q or symbol == arcade.key.E:
 			self.tanks[0].tr_speed = 0
+		if symbol == arcade.key.R or symbol == arcade.key.F:
+			self.tanks[0].aim_speed = 0.0
 
-	def add_tank(self, sprite_pos, x, y, angle):
-		tank = Tank(sprite_pos, x, y, angle)
+	def add_tank(self, sprite_pos, x, y, angle, max_ad):
+		tank = Tank(sprite_pos, x, y, angle, max_ad)
 		self.tank_sprites.append(tank.body_sprite)
 		self.tank_sprites.append(tank.turret_sprite)
 		self.tank_sprites.append(tank.turret_lock_sprite)
+		self.tank_sprites.append(tank.reticle_sprite)
+		self.tank_sprites.append(tank.crosshair_sprite)
 		self.tanks.append(tank)
 
 	def setup(self):
 		arcade.set_background_color((20, 20, 20))
-		self.add_tank(self.tank_sprite_dict["Tank 1"], 200, 200, 180)
+		self.add_tank(self.tank_sprite_dict["Tank 1"], SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 90, 500)
 
 if __name__ == '__main__':
 	app = TinyTreads()
