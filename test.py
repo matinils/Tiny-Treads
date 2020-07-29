f47ec88ae6619bb7981e6983e2a57e5468305d2c
Ben Endara <bendara88@gmail.com>
2020-07-29 00:25:41 -0500

    modified:   test.py

--- test.py
+++ test.py
@@ -7,15 +7,16 @@ RADIUS = 150
 
 
 class Welcome(arcade.Window):
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        arcade.set_background_color(arcade.color.ALLOY_ORANGE)
+	def __init__(self):
+		super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+		arcade.set_background_color(arcade.color.ALLOY_ORANGE)
 
-    def on_draw(self):
-        arcade.start_render()
-        arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE)
+	def on_draw(self):
+		arcade.start_render()
+		arcade.draw_circle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE)
 
 
 if __name__ == '__main__':
-    app = Welcome()
-    arcade.run()
+	app = Welcome()
+	arcade.run()
+	print("Test")
